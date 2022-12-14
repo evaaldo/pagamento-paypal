203a47e43d16c26e1f0a55c5d583f59dcaf84300
evaaldo <evaaldormj@gmail.com>
2022-12-14 12:27:18 -0300

    feat: adicionando formas de pagamento com paypal

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,11 +1,70 @@
 import './style.css'
+import { useState, useEffect, useRef } from 'react'
 
 const Home = () => {
 
+    const [paid, setPaid] = useState(false)
+    const [loaded, setLoaded] = useState(false)
+
+    let paypalRef = useRef()
+
+    const product = {
+        price: 65.00,
+        description: 'Cabelo e barba'
+    }
+
+    useEffect(() => {
+        const script = document.createElement('script')
+        const id = 'Ae629eqJyvA9eLFtv4wJWAmQ2ggM_IAsnAmTzYUaS0F-wUN3R42khxa9fr1_0-oy9yI2hcWGrrV4ajRH'
+        script.src = `https://www.paypal.com/sdk/js?currency=BRL&client-id=${id}`
+
+        script.addEventListener('load', () => setLoaded(true))
+
+        document.body.appendChild(script)
+
+        if(loaded) {
+            function loadButtonsAndLogicAboutPayment() {
+                setTimeout(() => {
+                    window.paypal
+                    .Buttons({
+                        createOrder: (data, actions) => {
+                            return actions.order.create({
+                                purchase_units: [
+                                {
+                                    description: product.description,
+                                    amount: {
+                                        currency_code: 'BRL',
+                                        value: product.price
+                                    }
+                                }
+                            ]
+                            });
+                        },
+                        onApprove: async (_,actions) => {
+                            const order = await actions.order.capture()
+
+                            setPaid(true)
+
+                            console.log(order)
+                        }
+                    })
+                    .render(paypalRef)
+                })
+            }
+            loadButtonsAndLogicAboutPayment()
+        }
+    })
+
     return(
-        <div className='home'>
-            <h1 className='home__titulo'>Página inicial</h1>
-        </div>
+        <>
+            {paid ? (<div>
+                            <h1 className='pagamento__texto'>Pagamento efetuado!</h1>
+                        </div>) : (
+                            <div className='pagamento'>
+                                <div ref={v => (paypalRef = v)}></div>
+                            </div>
+                        )}
+        </>
     )
 
 }
--- src/Pages/SobreMim/index.jsx
+++ src/Pages/SobreMim/index.jsx
@@ -1,11 +1,70 @@
 import './style.css'
+import { useState, useEffect, useRef } from 'react'
 
 const SobreMim = () => {
 
+    const [paid, setPaid] = useState(false)
+    const [loaded, setLoaded] = useState(false)
+
+    let paypalRef = useRef()
+
+    const product = {
+        price: 45.00,
+        description: 'Cabelo'
+    }
+
+    useEffect(() => {
+        const script = document.createElement('script')
+        const id = 'AbFV3tsrmKFE4Dc8SkWphnOcG3biYvXlN9QA1NG4vsGlTxCJ9ZtblCA2pq9qSRku-dbfHWc8Mw1q48gD'
+        script.src = `https://www.paypal.com/sdk/js?currency=BRL&client-id=${id}`
+
+        script.addEventListener('load', () => setLoaded(true))
+
+        document.body.appendChild(script)
+
+        if(loaded) {
+            function loadButtonsAndLogicAboutPayment() {
+                setTimeout(() => {
+                    window.paypal
+                    .Buttons({
+                        createOrder: (data, actions) => {
+                            return actions.order.create({
+                                purchase_units: [
+                                {
+                                    description: product.description,
+                                    amount: {
+                                        currency_code: 'BRL',
+                                        value: product.price
+                                    }
+                                }
+                            ]
+                            });
+                        },
+                        onApprove: async (_,actions) => {
+                            const order = await actions.order.capture()
+
+                            setPaid(true)
+
+                            console.log(order)
+                        }
+                    })
+                    .render(paypalRef)
+                })
+            }
+            loadButtonsAndLogicAboutPayment()
+        }
+    })
+
     return(
-        <div className='sobre-mim'>
-            <h1 className='sobre-mim__titulo'>Sobre mim</h1>
-        </div>
+        <>
+            {paid ? (<div>
+                            <h1 className='pagamento__texto'>Pagamento efetuado!</h1>
+                        </div>) : (
+                            <div className='pagamento'>
+                                <div ref={v => (paypalRef = v)}></div>
+                            </div>
+                        )}
+        </>
     )
 
 }
